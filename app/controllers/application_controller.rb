41c1a551758f8f271224b9f14a71ba8f7aa7d255
Arlisson Jaques <arlissonjaques@gmail.com>
2021-12-11 21:09:20 -0300

    token generation and validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+
+  def token_generate(data)
+    encode_token(data)
+  end
+
+  private
+
+  def auth_header
+    request.headers['Authorization']&.split&.last
+  end
+
+  def encode_token(data)
+    JWT.encode(data, ENV['SECRET_KEY'], 'HS256')
+  end
+
+  def decode_token
+    if auth_header
+      begin
+        JWT.decode(auth_header, ENV['SECRET_KEY'], true, { algorithm: 'HS256' })
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def request_authorization
+    render json: { 
+      sucess: false,
+      errors: ['Invalid token']
+    }, status: :unauthorized unless decode_token
+  end
 end
